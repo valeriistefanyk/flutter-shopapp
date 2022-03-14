358c37c02487d2609e11a16b162ccb1513113e41
Valerii Stefanyk <valeriistefanyk@gmail.com>
2022-03-14 14:27:48 +0200

    Use Consumer instead of Provider in ProductItem

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -8,7 +8,7 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -25,15 +25,18 @@ class ProductItem extends StatelessWidget {
             )),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,
-            ),
-            onPressed: () {
-              product.toggleFavoriteStatus();
-            },
-            color: Theme.of(context).colorScheme.secondary,
-          ),
+          leading: Consumer<Product>(
+              builder: (ctx, product, child) => IconButton(
+                    icon: Icon(
+                      product.isFavorite
+                          ? Icons.favorite
+                          : Icons.favorite_border,
+                    ),
+                    onPressed: () {
+                      product.toggleFavoriteStatus();
+                    },
+                    color: Theme.of(context).colorScheme.secondary,
+                  )),
           title: Text(
             product.title,
             textAlign: TextAlign.center,
