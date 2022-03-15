de32daa9a7e819c2060adfc16595b8ff235dc650
Valerii Stefanyk <valeriistefanyk@gmail.com>
2022-03-15 14:06:49 +0200

    Add Only Favorites filter

--- lib/screens/products_overview/products_grid.dart
+++ lib/screens/products_overview/products_grid.dart
@@ -4,12 +4,15 @@ import '../../providers/products.dart';
 import '../../widgets/product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
-  const ProductsGrid({Key? key}) : super(key: key);
+  final bool showFavorites;
+
+  const ProductsGrid(this.showFavorites, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
+    final products =
+        showFavorites ? productsData.favoriteItems : productsData.items;
 
     return GridView.builder(
       padding: const EdgeInsets.all(10),
--- lib/screens/products_overview/products_overview.dart
+++ lib/screens/products_overview/products_overview.dart
@@ -1,14 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'products_grid.dart';
+import '../../providers/products.dart';
 
-class ProductOverviewScreen extends StatelessWidget {
+enum FilterOptions {
+  favorites,
+  all,
+}
+
+class ProductOverviewScreen extends StatefulWidget {
   const ProductOverviewScreen({Key? key}) : super(key: key);
 
   @override
+  State<ProductOverviewScreen> createState() => _ProductOverviewScreenState();
+}
+
+class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
+  var _showOnlyFavarites = false;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('MyShop')),
-      body: const ProductsGrid(),
+      appBar: AppBar(
+        title: const Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: FilterOptions.favorites,
+              ),
+              const PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.all,
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                switch (selectedValue) {
+                  case FilterOptions.favorites:
+                    _showOnlyFavarites = true;
+                    break;
+                  case FilterOptions.all:
+                    _showOnlyFavarites = false;
+                    break;
+                }
+              });
+            },
+          )
+        ],
+      ),
+      body: ProductsGrid(_showOnlyFavarites),
     );
   }
 }
