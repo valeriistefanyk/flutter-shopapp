618ec71349758f4f5d8e9e5737597774ea53984f
Valerii Stefanyk <valeriistefanyk@gmail.com>
2022-03-19 13:16:48 +0200

    Add Snackbar to ProductItem

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -48,6 +48,20 @@ class ProductItem extends StatelessWidget {
             icon: const Icon(Icons.shopping_cart),
             onPressed: () {
               cart.addItem(product.id, product.title, product.price);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: const Text(
+                  'Addd item to cart!',
+                  textAlign: TextAlign.center,
+                ),
+                duration: const Duration(seconds: 2),
+                action: SnackBarAction(
+                  label: 'UNDO',
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  },
+                ),
+              ));
             },
             color: Theme.of(context).colorScheme.secondary,
           ),
