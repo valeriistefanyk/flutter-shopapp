42157ea6631d07d5a1c376b183e8efae4581a5e1
Valerii Stefanyk <valeriistefanyk@gmail.com>
2022-03-24 16:40:13 +0200

    Replace firebase APIKEY, create secret file

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,18 +1,18 @@
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import '../secrets.dart';
 
 class Auth with ChangeNotifier {
   //String _token;
   //DateTime _expiryDate;
   //String _userId;
-  static const _key = 'AIzaSyD7EkpmzLdMZscVweDCDPJrVBZBbg8yJng';
   static const _domain = 'https://identitytoolkit.googleapis.com';
 
   Future<http.Response> _authenticate(
       String email, String password, String urlSegment) async {
     return await http.post(
-        Uri.parse('$_domain/v1/accounts:$urlSegment?key=$_key'),
+        Uri.parse('$_domain/v1/accounts:$urlSegment?key=$FIREBASE_API_KEY'),
         body: json.encode(
             {'email': email, 'password': password, 'returnSecureToken': true}));
   }
